da0f937c5fe25a730850e5e92615767b259ebe37
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2011-07-03 16:25:35 +0100

    moving all callables to one place

--- src/com/googlecode/waitrest/Callables.java
+++ src/com/googlecode/waitrest/Callables.java
@@ -1,9 +1,11 @@
 package com.googlecode.waitrest;
 
 import com.googlecode.totallylazy.Callable1;
+import com.googlecode.utterlyidle.QueryParameters;
 import com.googlecode.utterlyidle.Request;
 import com.googlecode.utterlyidle.Response;
 import com.googlecode.utterlyidle.Status;
+import com.googlecode.utterlyidle.io.HierarchicalPath;
 
 public class Callables {
     public static Callable1<Request, String> method() {
@@ -15,11 +17,29 @@ public class Callables {
         };
     }
 
-    public static Callable1<Request, String> path() {
+    public static Callable1<Request, String> stringPath() {
         return new Callable1<Request, String>() {
             @Override
             public String call(Request request) throws Exception {
                 return request.url().path().toString();
+            }
+        };
+    }
+
+    public static Callable1<Request, HierarchicalPath> path() {
+        return new Callable1<Request, HierarchicalPath>() {
+            @Override
+            public HierarchicalPath call(Request request) throws Exception {
+                return request.url().path();
+            }
+        };
+    }
+
+    public static Callable1<Request, QueryParameters> query() {
+        return new Callable1<Request, QueryParameters>() {
+            @Override
+            public QueryParameters call(Request request) throws Exception {
+                return request.query();
             }
         };
     }
--- src/com/googlecode/waitrest/Predicates.java
+++ src/com/googlecode/waitrest/Predicates.java
@@ -13,7 +13,7 @@ import static com.googlecode.totallylazy.Predicates.where;
 
 public class Predicates {
     public static LogicalPredicate<Pair<Request, Response>> path(final String path) {
-        return where(first(Request.class), where(Callables.path(), is(path)));
+        return where(first(Request.class), where(Callables.stringPath(), is(path)));
     }
 
     public static LogicalPredicate<Pair<Request, Response>> status(final Status status) {
