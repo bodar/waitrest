222be0411ad90e3342a70ba710337db4ff00baa4
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2011-07-03 15:13:19 +0100

    basic error handling with error messages on the take order screen

--- src/com/googlecode/waitrest/Predicates.java
+++ src/com/googlecode/waitrest/Predicates.java
@@ -1,19 +1,28 @@
 package com.googlecode.waitrest;
 
 import com.googlecode.totallylazy.First;
+import com.googlecode.totallylazy.Pair;
+import com.googlecode.totallylazy.Second;
 import com.googlecode.totallylazy.predicates.LogicalPredicate;
 import com.googlecode.utterlyidle.Request;
+import com.googlecode.utterlyidle.Response;
+import com.googlecode.utterlyidle.Status;
 
 import static com.googlecode.totallylazy.Callables.first;
+import static com.googlecode.totallylazy.Callables.second;
 import static com.googlecode.totallylazy.Predicates.is;
 import static com.googlecode.totallylazy.Predicates.where;
 
 public class Predicates {
-    public static LogicalPredicate<First<Request>> path(final String path) {
+    public static LogicalPredicate<Pair<Request, Response>> path(final String path) {
         return where(first(Request.class), where(Callables.path(), is(path)));
     }
 
-    public static LogicalPredicate<First<Request>> method(final String method) {
+    public static LogicalPredicate<Pair<Request, Response>> status(final Status status) {
+        return where(second(Response.class), where(Callables.status(), is(status)));
+    }
+
+    public static LogicalPredicate<Pair<Request, Response>> method(final String method) {
         return where(first(Request.class), where(Callables.method(), is(method)));
     }
 }
