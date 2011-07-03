c967010e74bf67c173e9b7cedf2bb0518cc6a7e1
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2011-07-03 15:28:46 +0100

    e2e test for error handling

--- test/com/googlecode/waitrest/RestaurantTest.java
+++ test/com/googlecode/waitrest/RestaurantTest.java
@@ -1,9 +1,6 @@
 package com.googlecode.waitrest;
 
-import com.googlecode.utterlyidle.Request;
-import com.googlecode.utterlyidle.Response;
-import com.googlecode.utterlyidle.ServerConfiguration;
-import com.googlecode.utterlyidle.Status;
+import com.googlecode.utterlyidle.*;
 import com.googlecode.utterlyidle.handlers.ClientHttpHandler;
 import com.googlecode.utterlyidle.httpserver.RestServer;
 import org.junit.After;
@@ -19,8 +16,11 @@ import static com.googlecode.utterlyidle.RequestBuilder.get;
 import static com.googlecode.utterlyidle.RequestBuilder.post;
 import static com.googlecode.utterlyidle.RequestBuilder.put;
 import static com.googlecode.utterlyidle.Responses.response;
+import static com.googlecode.utterlyidle.Status.BAD_REQUEST;
+import static com.googlecode.utterlyidle.Status.NO_CONTENT;
 import static com.googlecode.utterlyidle.Status.OK;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
 
 public class RestaurantTest {
@@ -40,9 +40,10 @@ public class RestaurantTest {
 
     @Test
     public void serveRequestOrder() throws Exception {
-        Request put = put(server.getUrl() + "cheese").withQuery("type", "cheddar").withHeader(CONTENT_TYPE, TEXT_PLAIN).withHeader(CONTENT_LENGTH, "6").withInput("cheese".getBytes()).build();
-        Request get = get(server.getUrl() + "cheese").withQuery("type", "cheddar").build();
-        Request unknownGet = get(server.getUrl() + "cheese").withQuery("type", "gouda").build();
+        String cheeseUrl = server.getUrl() + "cheese";
+        Request put = put(cheeseUrl).withQuery("type", "cheddar").withHeader(CONTENT_TYPE, TEXT_PLAIN).withHeader(CONTENT_LENGTH, "6").withInput("cheese".getBytes()).build();
+        Request get = get(cheeseUrl).withQuery("type", "cheddar").build();
+        Request unknownGet = get(cheeseUrl).withQuery("type", "gouda").build();
         Response response = response(OK).bytes("cheese".getBytes()).header(CONTENT_TYPE, TEXT_PLAIN).header(CONTENT_LENGTH, "6");
 
         restClient.handle(put);
@@ -53,13 +54,24 @@ public class RestaurantTest {
 
     @Test
     public void serveRequestResponseOrder() throws Exception {
-        Request request = get(server.getUrl() + "cheese").withQuery("type", "cheddar").build();
-        Request unknownRequest = get(server.getUrl() + "cheese").withQuery("type", "gouda").build();
+        String cheeseUrl = server.getUrl() + "cheese";
+        Request request = get(cheeseUrl).withQuery("type", "cheddar").build();
+        Request unknownRequest = get(cheeseUrl).withQuery("type", "gouda").build();
         Response expectedResponse = response(OK).bytes("cheese".getBytes()).header(CONTENT_TYPE, TEXT_PLAIN).header(CONTENT_LENGTH, "6");
 
         restClient.handle(post(server.getUrl() + Waitress.WAITRESS_ORDER_PATH).withForm("request", request.toString()).withForm("response", expectedResponse.toString()).build());
 
         assertThat(restClient.handle(request), is(expectedResponse));
         assertThat(restClient.handle(unknownRequest).status(), is(Status.NOT_FOUND));
+    }
+
+    @Test
+    public void cannotTakeInvalidOrder() throws Exception {
+        Response response = restClient.handle(post(server.getUrl() + Waitress.WAITRESS_ORDER_PATH).
+                withForm("request", "invalidMethod /path HTTP/1.1").
+                withForm("response", response(NO_CONTENT).toString()).build());
+
+        assertThat(response.status(), is(BAD_REQUEST));
+        assertThat(response.toString(), containsString("Request without a method"));
     }
 }
