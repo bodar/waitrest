3e9658d5b54cf0bc2bfa746f08fdb89b8bac087d
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2011-08-09 20:02:37 +0100

    renaming class

--- src/com/googlecode/waitrest/Main.java
+++ src/com/googlecode/waitrest/Main.java
@@ -3,7 +3,7 @@ package com.googlecode.waitrest;
 import com.googlecode.utterlyidle.ServerConfiguration;
 import com.googlecode.utterlyidle.httpserver.RestServer;
 
-public class Building {
+public class Main {
 
     public static void main(String[] args) throws Exception {
         new RestServer(new Restaurant(), ServerConfiguration.defaultConfiguration().port(8899));
