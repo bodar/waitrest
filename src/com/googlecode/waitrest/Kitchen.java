d0534e65329c1491add89a057a34fc99724c53b2
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2011-08-09 19:58:14 +0100

    upgrading tl and uttidle, one predicate moved to tl

--- src/com/googlecode/waitrest/Kitchen.java
+++ src/com/googlecode/waitrest/Kitchen.java
@@ -11,7 +11,6 @@ import java.util.Map;
 import static com.googlecode.totallylazy.Predicates.*;
 import static com.googlecode.totallylazy.Sequences.sequence;
 import static com.googlecode.utterlyidle.HttpHeaders.CONTENT_TYPE;
-import static com.googlecode.waitrest.Predicates.containedBy;
 
 public class Kitchen {
     private Map<Request, Response> orders = new HashMap<Request, Response>();
@@ -29,8 +28,8 @@ public class Kitchen {
     public Option<Response> serve(Request request) {
         return sequence(orders.keySet()).
                 filter(where(Requests.path(), is(request.url().path()))).
-                filter(where(Requests.form(), containedBy(request.form()))).
-                filter(where(Requests.query(), containedBy(request.query()))).
+                filter(where(Requests.form(), subsetOf(request.form()))).
+                filter(where(Requests.query(), subsetOf(request.query()))).
                 filter(where(Requests.method(), or(is(request.method()), is(HttpMethod.PUT)))).
                 headOption().
                 map(response());
