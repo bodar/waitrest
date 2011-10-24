3bac15f6a550c13950cfd99f0b2f1e7390437e49
dan <dan@bodar.com>
2011-10-24 11:34:32 +0100

    Updated TL version

--- src/com/googlecode/waitrest/Main.java
+++ src/com/googlecode/waitrest/Main.java
@@ -4,9 +4,7 @@ import com.googlecode.utterlyidle.ServerConfiguration;
 import com.googlecode.utterlyidle.httpserver.RestServer;
 
 public class Main {
-
     public static void main(String[] args) throws Exception {
         new RestServer(new Restaurant(), ServerConfiguration.defaultConfiguration().port(8899));
     }
-
 }
