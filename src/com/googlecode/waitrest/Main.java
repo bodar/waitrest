42d1f3263b970349fe1ff237dfb72cdf05c594a1
Mateusz Kwasniewski <Mateusz Kwasniewski@localhost>
2011-12-15 13:38:44 +0000

    Embedded dependencies on googlecode libs

--- src/com/googlecode/waitrest/Main.java
+++ src/com/googlecode/waitrest/Main.java
@@ -1,10 +1,7 @@
 package com.googlecode.waitrest;
 
-import static com.googlecode.utterlyidle.ApplicationBuilder.application;
-import static com.googlecode.utterlyidle.ServerConfiguration.defaultConfiguration;
-
 public class Main {
     public static void main(String[] args) throws Exception {
-        application(Restaurant.class).start(defaultConfiguration().port(8899));
+        new Waitrest();
     }
 }
