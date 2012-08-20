94832cf0e2e577cb7d396e486fd2dd2efa74d020
dan <dan@bodar.com>
2012-08-20 13:41:33 +0100

    Made waitrest shut down application

--- src/com/googlecode/waitrest/Waitrest.java
+++ src/com/googlecode/waitrest/Waitrest.java
@@ -3,17 +3,20 @@ package com.googlecode.waitrest;
 import com.googlecode.utterlyidle.BasePath;
 import com.googlecode.utterlyidle.httpserver.RestServer;
 
+import java.io.Closeable;
 import java.io.IOException;
 import java.net.URL;
 
 import static com.googlecode.utterlyidle.ServerConfiguration.defaultConfiguration;
 
-public class Waitrest {
+public class Waitrest implements Closeable {
     private RestServer restServer;
+    private Restaurant application;
 
     public Waitrest(String basePath, Integer port) {
         try {
-            restServer = new RestServer(new Restaurant(BasePath.basePath(basePath)), port == null ?  defaultConfiguration() : defaultConfiguration().port(port));
+            application = new Restaurant(BasePath.basePath(basePath));
+            restServer = new RestServer(application, port == null ?  defaultConfiguration() : defaultConfiguration().port(port));
         } catch (Exception e) {
             throw new RuntimeException("Couldn't start Waitrest: " + e.getMessage());
         }
@@ -29,6 +32,7 @@ public class Waitrest {
 
     public void close() {
         try {
+            application.close();
             restServer.close();
         } catch (IOException e) {
             throw new RuntimeException("Couldn't stop Waitrest: " + e.getMessage());
